35c298a566bfc2add97775e35f6aa69e919549bd
Nico <nicoalearriola@gmail.com>
2024-10-02 23:53:56 -0300

    primeras implementaciones de la page Friend - contador, busqueda, agregar invitador [es estatico, todavia no es funcional]

--- src/views/Friends/Friends.tsx
+++ src/views/Friends/Friends.tsx
@@ -1,50 +1,97 @@
 import React from "react";
 import { StyleSheet, Text, View, ScrollView, } from "react-native";
-import { Button, Icon, Input } from "@rneui/themed";
+import { Button, Icon } from "@rneui/themed";
 
 import Headers from '../../components/Header/Header'
+import { Image } from "react-native-elements";
 
 const Friends = () => {
     return(
         <ScrollView> 
             <View style={styles.container}>
-                <View>
-                    <Headers/>
-                </View>
-                <View style={styles.searchContainer}>
-                    <View style={styles.inputContainer}>
+                <Headers/>
 
+                <View style={styles.listContainer}>
+                    <View style={styles.leftContainer}>
+                        <Text style={styles.listLegend}>Lista de invitados</Text>
+                    </View>
+                    <View style={styles.rightContainer}>
+                        <Text style={styles.counterNum}>15</Text>
+                    </View>
+                </View>
+
+                <View style={styles.listContainer}>
+                    <View style={styles.leftContainer}>
+                        <Text style={styles.listLegend}>buscador...</Text>
                     </View>
                     <View>
-                        <Text style={styles.searchBtContainer}>buscador</Text>
+                        <Text style={styles.listLegend}>Icono</Text>
                     </View>
                 </View>
+
+                {/* METODO LOCAL PARA AGREGAR USUARIOS */}
+
+                <View style={styles.addFriendContainer}>
+                    <View style={styles.leftAddContainer}>
+                        <Text style={styles.listLegend}>Agregar Invitados</Text>
+                    </View>
+                    <View style={styles.rightAddContainer}>
+                        <Button
+                            
+                        />
+                    </View>
+                </View>
+
             </View>
         </ScrollView>
     )
 }
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 12,
     },
-    searchContainer: {
+    listContainer:{
+        flexDirection:'row',
+        alignItems:'center',
+        marginVertical:15,
+    },
+    leftContainer:{
+        flex:1,
+        justifyContent:'center',
+
+    },
+    listLegend:{
+        color:'#ffff',
+        fontSize:16,
+        fontFamily:'RobotoMono-Regular',
+    },
+    rightContainer:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor: '#ffff', // Fondo rojo para el círculo
+        width: 30,
+        height: 30, 
+        borderRadius: 15,
+    },
+    counterNum:{
+        color:'#C21807',
+        fontFamily:'RobotoMono-Medium',
+        fontSize:16,
+    },
+    addFriendContainer:{
         alignItems: "center",
         flexDirection: "row",
-      },
-      inputContainer: {
-        flex: 1,
-        marginLeft: -12,
-      },
-      searchBtContainer: {
-        color: "#ffff",
-        fontSize: 14,
-      },
-      content:{
+        marginVertical:15,
+    },
+    leftAddContainer:{
         flex:1,
-        marginTop: 16,
-      },
+    },
+    rightAddContainer:{},
+    
 })
 
 export default Friends
